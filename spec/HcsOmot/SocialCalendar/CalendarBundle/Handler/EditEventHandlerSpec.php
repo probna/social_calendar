526c6dea6b2944fce8f2788c9e30acbbb7e0f0dc
Tomo Šala <hcs.omot@gmail.com>
2017-09-07 20:26:34 +0200

    Refactor spec and clean up code

--- spec/HcsOmot/SocialCalendar/CalendarBundle/Handler/EditEventHandlerSpec.php
+++ spec/HcsOmot/SocialCalendar/CalendarBundle/Handler/EditEventHandlerSpec.php
@@ -4,24 +4,59 @@ namespace spec\HcsOmot\SocialCalendar\CalendarBundle\Handler;
 
 use Doctrine\ORM\EntityManager;
 use HcsOmot\SocialCalendar\CalendarBundle\Command\EditEventCommand;
+use HcsOmot\SocialCalendar\CalendarBundle\Entity\Event;
 use HcsOmot\SocialCalendar\CalendarBundle\Handler\EditEventHandler;
+use HcsOmot\SocialCalendar\CalendarBundle\Repository\EventRepository;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 
-class EditEventHandlerSpec extends ObjectBehavior
-{
+class EditEventHandlerSpec extends ObjectBehavior {
     function it_is_initializable()
     {
         $this->shouldHaveType(EditEventHandler::class);
     }
 
-    public function let(EntityManager $entityManager)
+    public function let(EntityManager $entityManager, EventRepository $eventRepository)
     {
-        $this->beConstructedWith($entityManager);
+
+        $this->beConstructedWith($entityManager, $eventRepository);
+
+
     }
 
-    public function it_should_handle_command(EditEventCommand $editEventCommand)
+    public function it_should_handle_command(
+        EditEventCommand $editEventCommand,
+        EntityManager $entityManager,
+        EventRepository $eventRepository,
+        Event $event
+    )
     {
+
+        $editEventCommand->getId()->shouldBeCalled()->willReturn(2);
+        $editEventCommand->getDescription()->shouldBeCalled()->willReturn('bla');
+        $editEventCommand->getVenue()->shouldBeCalled()->willReturn('bla2');
+
+        $event->setDescription("bla")->shouldBeCalled();
+        $event->setVenue("bla2")->shouldBeCalled();
+        $entityManager->persist($event)->shouldBeCalled();
+        $entityManager->flush()->shouldBeCalled();
+        $eventRepository->find(2)->shouldBeCalled()->willReturn($event);
+
+
         $this->handle($editEventCommand);
+    }
+
+    public function it_should_X(
+        EditEventCommand $editEventCommand,
+        EventRepository $eventRepository
+    )
+    {
+
+        $editEventCommand->getId()->shouldBeCalled()->willReturn(2);
+
+        $eventRepository->find(2)->shouldBeCalled()->willReturn(null);
+
+
+        $this->shouldThrow(\Exception::class)->duringHandle($editEventCommand);
+
     }
 }
