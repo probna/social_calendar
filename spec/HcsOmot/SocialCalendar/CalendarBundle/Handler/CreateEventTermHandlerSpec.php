b89354594e8efc38967a17109e94075d740e28aa
Tomo Šala <hcs.omot@gmail.com>
2017-09-18 19:04:17 +0200

    Refactor CreateEventTerm Handler & Spec
    Refactored CreateEventTermHandler & Spec to work with Command that
    only deals with ID representations of entities.

--- spec/HcsOmot/SocialCalendar/CalendarBundle/Handler/CreateEventTermHandlerSpec.php
+++ spec/HcsOmot/SocialCalendar/CalendarBundle/Handler/CreateEventTermHandlerSpec.php
@@ -3,10 +3,13 @@
 namespace spec\HcsOmot\SocialCalendar\CalendarBundle\Handler;
 
 use AppBundle\Entity\User;
+use AppBundle\Repository\UserRepository;
 use Doctrine\ORM\EntityManager;
 use HcsOmot\SocialCalendar\CalendarBundle\Command\CreateEventTermCommand;
 use HcsOmot\SocialCalendar\CalendarBundle\Entity\Event;
+use HcsOmot\SocialCalendar\CalendarBundle\Entity\EventTerm;
 use HcsOmot\SocialCalendar\CalendarBundle\Handler\CreateEventTermHandler;
+use HcsOmot\SocialCalendar\CalendarBundle\Repository\EventRepository;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -17,17 +20,26 @@ class CreateEventTermHandlerSpec extends ObjectBehavior
         $this->shouldHaveType(CreateEventTermHandler::class);
     }
 
-    public function let(EntityManager $entityManager)
+    public function let(EntityManager $entityManager, EventRepository $eventRepository, UserRepository $userRepository)
     {
-        $this->beConstructedWith($entityManager);
+        $this->beConstructedWith($entityManager, $eventRepository, $userRepository);
     }
 
-    public function it_should_be_able_to_handle_create_eventterm_command(CreateEventTermCommand $createEventTermCommand, Event $event, \DateTime $term, User $proposer)
+    public function it_should_be_able_to_handle_create_eventterm_command(CreateEventTermCommand $createEventTermCommand, EventRepository $eventRepository, Event $event, \DateTime $term, UserRepository $userRepository, User $eventTermProposer, EntityManager $entityManager)
     {
-        $createEventTermCommand->getId()->willReturn(5266);
-        $createEventTermCommand->getEvent()->willReturn($event);
-        $createEventTermCommand->getTerm()->willReturn($term);
-        $createEventTermCommand->getProposer()->willReturn($proposer);
+        $createEventTermCommand->getId()->shouldBeCalled()->willReturn(5266);
+        $createEventTermCommand->getEventId()->shouldBeCalled()->willReturn(6);
+
+        $eventRepository->find(6)->shouldBeCalled()->willReturn($event);
+
+        $createEventTermCommand->getTerm()->shouldBeCalled()->willReturn($term);
+
+        $createEventTermCommand->getProposerId()->shouldBeCalled()->willReturn(1);
+
+        $userRepository->find(1)->shouldBeCalled()->willReturn($eventTermProposer);
+
+        $entityManager->persist(Argument::type(EventTerm::class))->shouldBeCalled();
+        $entityManager->flush()->shouldBeCalled();
 
         $this->handle($createEventTermCommand);
     }
--- src/HcsOmot/SocialCalendar/CalendarBundle/Handler/CreateEventTermHandler.php
+++ src/HcsOmot/SocialCalendar/CalendarBundle/Handler/CreateEventTermHandler.php
@@ -2,9 +2,11 @@
 
 namespace HcsOmot\SocialCalendar\CalendarBundle\Handler;
 
+use AppBundle\Repository\UserRepository;
 use Doctrine\ORM\EntityManager;
 use HcsOmot\SocialCalendar\CalendarBundle\Command\CreateEventTermCommand;
 use HcsOmot\SocialCalendar\CalendarBundle\Entity\EventTerm;
+use HcsOmot\SocialCalendar\CalendarBundle\Repository\EventRepository;
 
 class CreateEventTermHandler
 {
@@ -12,20 +14,34 @@ class CreateEventTermHandler
      * @var \Doctrine\ORM\EntityManager
      */
     private $entityManager;
+    /**
+     * @var \HcsOmot\SocialCalendar\CalendarBundle\Repository\EventRepository
+     */
+    private $eventRepository;
+    /**
+     * @var \AppBundle\Repository\UserRepository
+     */
+    private $userRepository;
 
-    public function __construct(EntityManager $entityManager)
-    {
-        $this->entityManager = $entityManager;
+    public function __construct(
+        EntityManager $entityManager,
+        EventRepository $eventRepository,
+        UserRepository $userRepository
+    ) {
+        $this->entityManager   = $entityManager;
+        $this->eventRepository = $eventRepository;
+        $this->userRepository  = $userRepository;
     }
 
     public function handle(CreateEventTermCommand $createEventTermCommand)
     {
-        $eventTermId       = $createEventTermCommand->getId();
-        $event             = $createEventTermCommand->getEvent();
-        $term              = $createEventTermCommand->getTerm();
-        $eventTermProposer = $createEventTermCommand->getProposer();
-
-        $eventTerm         = new EventTerm($eventTermId, $event, $term, $eventTermProposer);
+        $eventTermId         = $createEventTermCommand->getId();
+        $eventId             = $createEventTermCommand->getEventId();
+        $event               = $this->eventRepository->find($eventId);
+        $term                = $createEventTermCommand->getTerm();
+        $eventTermProposerId = $createEventTermCommand->getProposerId();
+        $eventTermProposer   = $this->userRepository->find($eventTermProposerId);
+        $eventTerm           = new EventTerm($eventTermId, $event, $term, $eventTermProposer);
 
         $this->entityManager->persist($eventTerm);
         $this->entityManager->flush();
