3a95044dbc58902046367900d47fb8d328aba782
Tomo Šala <hcs.omot@gmail.com>
2017-10-03 15:50:29 +0200

    WIP make event handle voting for a particular tem
    Event should make sure that the Voter is listed as event attendee, and
    only then allow voting (delegate the call to the EventTerm to handle it's
    own voting logic).

--- spec/HcsOmot/SocialCalendar/CalendarBundle/Entity/EventSpec.php
+++ spec/HcsOmot/SocialCalendar/CalendarBundle/Entity/EventSpec.php
@@ -3,13 +3,15 @@
 namespace spec\HcsOmot\SocialCalendar\CalendarBundle\Entity;
 
 use AppBundle\Entity\User;
+use Behat\Testwork\Counter\Timer;
 use HcsOmot\SocialCalendar\CalendarBundle\Entity\Event;
+use HcsOmot\SocialCalendar\CalendarBundle\Entity\EventTerm;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class EventSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType(Event::class);
     }
@@ -17,15 +19,13 @@ class EventSpec extends ObjectBehavior
     public function let(User $owner)
     {
         $this->beConstructedWith($id = 1234, $name = 'fun party', $description = 'descr', $venue = 'venue', $owner);
-
-
     }
 
     public function it_should_add_term_to_event(\DateTime $when, User $proposer)
     {
         $this->addAttendee($proposer);
 
-        $this->addTerm(time(),$when, $proposer);
+        $this->addTerm(time(), $when, $proposer);
 
         $collectionOfTerms = $this->getCandidateTerms();
 
@@ -72,9 +72,7 @@ class EventSpec extends ObjectBehavior
         $attendees = $this->getAttendees();
 
         $attendees->count()->shouldBe(1);
-
     }
-
 
     public function it_should_not_add_same_attendee_multiple_times(User $attendee)
     {
@@ -84,7 +82,42 @@ class EventSpec extends ObjectBehavior
         $attendees = $this->getAttendees();
 
         $attendees->count()->shouldBe(1);
+    }
+
+    public function it_should_allow_an_attendee_to_vote_for_an_event_term(\DateTime $term, User $termVoter, User $termProposer)
+    {
+        $this->addAttendee($termVoter);
+        $this->addAttendee($termProposer);
+
+        $this->addTerm(15, $term, $termProposer);
+        $this->voteForTerm(15, $termVoter);
+
+        $this->getEventTermVotersCount(15)->shouldReturn(2);
+    }
+
+    public function it_should_not_allow_non_attendees_to_vote(User $nonAttendee)
+    {
+        $this->shouldThrow(\DomainException::class)->duringVoteForTerm(15, $nonAttendee);
+    }
+
+    public function it_should_not_allow_an_attendee_to_vote_for_an_nonexisten_event_term(User $termVoter)
+    {
+        $this->addAttendee($termVoter);
+
+
+        $this->shouldThrow(\DomainException::class)->duringVoteForTerm(15, $termVoter);
+
 
     }
 
+    public function it_should_not_have_effect_if_term_proposer_tries_to_vote_for_proposed_term(\DateTime $term, User $termProposer)
+    {
+        $this->addAttendee($termProposer);
+        
+        $this->addTerm(15, $term, $termProposer);
+
+        $this->voteForTerm(15, $termProposer);
+
+        $this->getEventTermVotersCount(15)->shouldBe(1);
+    }
 }
