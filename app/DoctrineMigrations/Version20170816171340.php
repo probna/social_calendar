6335d84dc620f6f151132a48b170322be018425c
Tomo Šala <hcs.omot@gmail.com>
2017-10-03 22:08:54 +0200

    Run php-cs-fixer project wide

--- app/DoctrineMigrations/Version20170816171340.php
+++ app/DoctrineMigrations/Version20170816171340.php
@@ -16,7 +16,7 @@ class Version20170816171340 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE term_voters (event_term_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_EAC9937C8B7B9C53 (event_term_id), INDEX IDX_EAC9937CA76ED395 (user_id), PRIMARY KEY(event_term_id, user_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE term_voters ADD CONSTRAINT FK_EAC9937C8B7B9C53 FOREIGN KEY (event_term_id) REFERENCES event_term (id) ON DELETE CASCADE');
@@ -29,7 +29,7 @@ class Version20170816171340 extends AbstractMigration
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE term_voters');
     }
