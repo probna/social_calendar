1219f369f4ffbf952c2066f61dcf74d80366f3fa
Tomo Šala <hcs.omot@gmail.com>
2017-10-31 11:48:51 +0100

    Run phpcsfixer
    Php-cs-fixer project wide

--- app/DoctrineMigrations/Version20170809184548.php
+++ app/DoctrineMigrations/Version20170809184548.php
@@ -16,7 +16,7 @@ class Version20170809184548 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE event_term (id INT AUTO_INCREMENT NOT NULL, event_id INT NOT NULL, term DATETIME NOT NULL, term_score DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE event (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, venue VARCHAR(255) NOT NULL, event_term DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
@@ -28,7 +28,7 @@ class Version20170809184548 extends AbstractMigration
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE event_term');
         $this->addSql('DROP TABLE event');
